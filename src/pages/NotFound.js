5071176feb327ee123f1082d0abbbe257b509acd
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-25 11:15:24 +0200

    added user not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom"
+import Button from "../components/Button"
+import Container from "../components/Container"
+import H1 from "../components/H1"
+
 const NotFound = () => {
-  return 'Not found'
+  return(
+    <Container>
+      <H1>Page not found</H1>
+      <Link to={'/'} className="flex items-center justify-center md:m-20 my-20">
+        <Button text="Go to Home" className="text-2xl"/>
+      </Link>
+    </Container>
+  )
 }
 
 export default NotFound
